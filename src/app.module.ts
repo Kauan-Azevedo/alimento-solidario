ab07270415febdbb95d6352725d88ce0ea15f6ba
Kauan Azevedo <kauan.azevedo@fontespromotora.com.br>
2022-10-19 13:43:42 -0300

    module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config'
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRoot(process.env.MONGODB_URL),
     AlimentosModule
   ],
