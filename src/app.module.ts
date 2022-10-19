b99fdcd21e8ae4b349aa8b9cba2533129a3eb7a0
Kauan Azevedo <kauan.azevedo@fontespromotora.com.br>
2022-10-19 10:42:53 -0300

    feat: struct

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlimentosModule } from './alimentos/alimentos.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [AlimentosModule],
+  imports: [
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    AlimentosModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
