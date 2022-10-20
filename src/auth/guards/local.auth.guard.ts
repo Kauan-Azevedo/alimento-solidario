7e5af42548452637b8aa4aa7c84e3ca2651f770b
Kauan Azevedo <kauan.azevedo@fontespromotora.com.br>
2022-10-20 17:07:59 -0300

    video base

--- src/auth/guards/local.auth.guard.ts
+++ src/auth/guards/local.auth.guard.ts
@@ -3,5 +3,5 @@ import { AuthGuard } from '@nestjs/passport'
 
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {
-
+    // https://www.youtube.com/watch?v=3z6Cs_PtYc0&t=2536s&ab_channel=PauloSalvatore
 }
